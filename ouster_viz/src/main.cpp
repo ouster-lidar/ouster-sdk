e890d32608ca058da3e2915813e168c93c6f9985
Dima Garbuzov <dima.garbuzov@ouster.io>
2019-05-02 14:11:59 -0700

    Update for fw 1.12. See changelog for details
    * Firmware >= 1.12 will flip the sign of accelerations reported by the IMU
    * Add install directives for ouster_ros CMakeLists.txt
    * Fix visualizer issue with pointcloud not displaying with newer Eigen

--- ouster_viz/src/main.cpp
+++ ouster_viz/src/main.cpp
@@ -5,9 +5,11 @@
 #include <cstdlib>
 
 #include <Eigen/Eigen>
+#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <thread>
 #include <vector>
@@ -24,21 +26,41 @@ namespace viz = ouster::viz;
  * Print usage
  */
 void print_help() {
-    std::cout << "Usage: viz [options] [hostname] [udp_destination]\n"
-              << "Options:\n"
-              << "  -m <512x10 | 512x20 | 1024x10 | 1024x20 | 2048x10> "
-                 "lidar mode, default: 1024x10"
-              << std::endl;
+    std::cout
+        << "Usage: viz [options] [hostname] [udp_destination]\n"
+        << "Options:\n"
+        << "  -m <512x10 | 512x20 | 1024x10 | 1024x20 | 2048x10> : lidar mode, "
+           "default 1024x10\n"
+        << "  -f <path> : use provided metadata file; do not configure via TCP"
+        << std::endl;
+}
+
+std::string read_metadata(const std::string& meta_file) {
+    std::stringstream buf{};
+    std::ifstream ifs{};
+    ifs.open(meta_file);
+    buf << ifs.rdbuf();
+    ifs.close();
+
+    if (!ifs) {
+        std::cerr << "Failed to read " << meta_file
+                  << "; check that the path is valid" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    return buf.str();
 }
 
 int main(int argc, char** argv) {
     int W = 1024;
     int H = OS1::pixels_per_column;
     OS1::lidar_mode mode = OS1::MODE_1024x10;
+    bool do_config = true;  // send tcp commands to configure sensor
+    std::string metadata{};
 
     try {
         int c = 0;
-        while ((c = getopt(argc, argv, "hm:")) != -1) {
+        while ((c = getopt(argc, argv, "hm:f:")) != -1) {
             switch (c) {
                 case 'h':
                     print_help();
@@ -56,6 +78,10 @@ int main(int argc, char** argv) {
                         std::exit(EXIT_FAILURE);
                     }
                     break;
+                case 'f':
+                    do_config = false;
+                    metadata = read_metadata(optarg);
+                    break;
                 case '?':
                     std::cout << "Invalid Argument Format" << std::endl;
                     print_help();
@@ -69,19 +95,24 @@ int main(int argc, char** argv) {
         std::exit(EXIT_FAILURE);
     }
 
-    if (argc != optind + 2) {
+    if (do_config && argc != optind + 2) {
         std::cerr << "Expected 2 arguments after options" << std::endl;
         print_help();
         std::exit(EXIT_FAILURE);
     }
 
-    std::cout << "Sensor: " << argv[optind]
-              << " UDP Destination:" << argv[optind + 1] << std::endl;
     std::shared_ptr<OS1::client> cli;
-    cli = OS1::init_client(argv[optind], argv[optind + 1], mode);
+    if (do_config) {
+        std::cout << "Configuring sensor: " << argv[optind]
+                  << " UDP Destination:" << argv[optind + 1] << std::endl;
+        cli = OS1::init_client(argv[optind], argv[optind + 1], mode);
+    } else {
+        std::cout << "Listening for sensor data" << std::endl;
+        cli = OS1::init_client();
+    }
+
     if (!cli) {
-        std::cerr << "Failed to connect to client at: " << argv[optind]
-                  << std::endl;
+        std::cerr << "Failed to initialize client" << std::endl;
         print_help();
         std::exit(EXIT_FAILURE);
     }
@@ -93,7 +124,9 @@ int main(int argc, char** argv) {
 
     auto vh = viz::init_viz(W, H);
 
-    auto info = OS1::parse_metadata(get_metadata(*cli));
+    if (do_config) metadata = OS1::get_metadata(*cli);
+
+    auto info = OS1::parse_metadata(metadata);
 
     auto xyz_lut = OS1::make_xyz_lut(W, H, info.beam_azimuth_angles,
                                      info.beam_altitude_angles);
@@ -106,8 +139,8 @@ int main(int argc, char** argv) {
 
     // callback that calls update with filled lidar scan
     auto batch_and_update = OS1::batch_to_iter<ouster::LidarScan::iterator>(
-        xyz_lut, W, H, {}, &ouster::LidarScan::make_val,
-        [&](uint64_t) {
+        xyz_lut, W, H, ouster::LidarScan::Point::Zero(),
+        &ouster::LidarScan::make_val, [&](uint64_t) {
             // swap lidar scan and point it to new buffer
             viz::update(*vh, ls);
             it = ls->begin();
