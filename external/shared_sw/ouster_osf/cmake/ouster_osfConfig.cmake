e5772e282069238382bb84d9b2269a77373fdcdc
Pavlo Bashmakov <pavlo.bashmakov@ouster.io>
2020-09-29 00:33:12 +0000

    Merged in pb/osf-not-in-shared-sw (MR !1651)
    Move ouster_osf from 'shared_sw/ouster_osf' to 'lib/ouster_osf'.
    
    Approved-by: Dima Garbuzov <dima.garbuzov@ouster.io>
    Approved-by: Kairen Wong <kai.wong@ouster.io>
    Approved-by: Hao Yuan <hao.yuan@ouster.io>

--- external/shared_sw/ouster_osf/cmake/ouster_osfConfig.cmake
+++
@@ -1,5 +0,0 @@
-# Guard against multiple includes
-if(NOT TARGET OUSTER_OSF_INCLUDED)
-  add_custom_target(OUSTER_OSF_INCLUDED)
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../ ${CMAKE_CURRENT_BINARY_DIR}/ouster_osf)
-endif()
--- external/shared_sw/ouster_osf/include/ouster/osf/common.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Generated enums from shared fbs schemas
-// This is the only Flatbuffers generated code exposed to the libosf users
-#include "osfCommon_generated.h"
