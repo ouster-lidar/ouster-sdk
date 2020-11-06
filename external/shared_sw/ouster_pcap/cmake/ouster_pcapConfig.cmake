517686a7ba10779770dda69d950d6b35dded7c58
Dima Garbuzov <dima.garbuzov@ouster.io>
2020-11-06 01:26:20 +0000

    Finish removing shared_sw and cleaning up CI (MR !1716)
    Move remaining libraries (including ouster_example) under lib/ and build
    files to apps/sensor-client-integration. Remove as many references to
    shared_sw as possible. Update CI accordingly and implement various
    quality-of-life improvements:
    
    * New CI job named "sensor-client" to replace "shared_sw"
    * Update pipeline to checkout to subdirectory
    * Update CI build to avoid building in-tree
    * Update Python CI to use 3.7
    * Add artifact archiving and test result reporting to CI
    
    Approved-by: Chris Bayruns
    Approved-by: Pavlo Bashmakov
    Approved-by: Roy Rinberg

--- external/shared_sw/ouster_pcap/cmake/ouster_pcapConfig.cmake
+++
@@ -1,4 +0,0 @@
-if(NOT TARGET OUSTER_PCAP_INCLUDED)
-  add_custom_target(OUSTER_PCAP_INCLUDED)
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../ ${CMAKE_CURRENT_BINARY_DIR}/ouster_pcap)
-endif()
--- external/shared_sw/python-sensor-tools/tests/test_checker.py
+++
@@ -1,5 +0,0 @@
-from ouster.cli import checker, example  # noqa
-
-
-def test_nothing():
-    assert True
