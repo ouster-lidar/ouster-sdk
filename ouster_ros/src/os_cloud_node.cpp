f7b487e6f9003e23215a68da1a7b69351b642d08
andrimam <andrimam@amazon.com>
2021-04-25 13:19:20 -0700

    allow changing ROS tf frame names via params:
    * sensor_frame
    * imu_frame
    * lidar_frame
    
    and expose as ROS launch args.

--- ouster_ros/src/os_cloud_node.cpp
+++ ouster_ros/src/os_cloud_node.cpp
@@ -31,9 +31,9 @@ int main(int argc, char** argv) {
 
     auto tf_prefix = nh.param("tf_prefix", std::string{});
     if (!tf_prefix.empty() && tf_prefix.back() != '/') tf_prefix.append("/");
-    auto sensor_frame = tf_prefix + "os_sensor";
-    auto imu_frame = tf_prefix + "os_imu";
-    auto lidar_frame = tf_prefix + "os_lidar";
+    auto sensor_frame = tf_prefix + nh.param("sensor_frame", std::string("os_sensor"));
+    auto imu_frame = tf_prefix + nh.param("imu_frame", std::string("os_imu"));
+    auto lidar_frame = tf_prefix + nh.param("lidar_frame", std::string("os_lidar"));
 
     ouster_ros::OSConfigSrv cfg{};
     auto client = nh.serviceClient<ouster_ros::OSConfigSrv>("os_config");
