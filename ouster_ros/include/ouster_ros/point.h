9e5db6609e78c95d7d67f2acd06d1eaec7a07291
Kunal Tyagi <tyagi.kunal@live.com>
2022-02-23 14:16:06 +0900

    Update Point.h to reduce memory footprint
    Old footprint: 48 bytes
    New footprint: 32 byts

--- ouster_ros/include/ouster_ros/point.h
+++ ouster_ros/include/ouster_ros/point.h
@@ -18,11 +18,11 @@ namespace ouster_ros {
 struct EIGEN_ALIGN16 Point {
     PCL_ADD_POINT4D;
     float intensity;
+    uint32_t range;
     uint32_t t;
     uint16_t reflectivity;
-    uint8_t ring;
     uint16_t ambient;
-    uint32_t range;
+    uint8_t ring;
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 }  // namespace ouster_ros
@@ -34,10 +34,10 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
     (float, z, z)
     (float, intensity, intensity)
     // use std::uint32_t to avoid conflicting with pcl::uint32_t
+    (std::uint32_t, range, range)
     (std::uint32_t, t, t)
     (std::uint16_t, reflectivity, reflectivity)
-    (std::uint8_t, ring, ring)
     (std::uint16_t, ambient, ambient)
-    (std::uint32_t, range, range)
+    (std::uint8_t, ring, ring)
 )
 // clang-format on
