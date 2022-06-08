f91a0796cc31ff33d3382a0688052e0143e0df8b
Kai Wong <kai.wong@ouster.io>
2022-06-07 22:50:29 -0700

    Update version numbers

--- python/setup.py
+++ python/setup.py
@@ -124,7 +124,7 @@ class sdk_bdist_wheel(bdist_wheel):
 setup(
     name='ouster-sdk',
     url='https://github.com/ouster-lidar/ouster_example',
-    version='0.4.0',
+    version='0.4.1',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     namespace_packages=['ouster'],
