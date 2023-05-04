a1736a93bdba4c8c2d7a79fc2d723618537e0768
Kai Wong <44489146+kairenw@users.noreply.github.com>
2023-05-04 15:19:05 -0700

    Test 2

--- .github/check_title_and_description.py
+++ .github/check_title_and_description.py
@@ -8,7 +8,7 @@ description_re = r'((\n[\20-\x7E]{0,100})+)?(\n)?'
 num_commits = int(os.environ.get("NUM_COMMITS"))
 title = os.environ.get("TITLE")
 pr_number_string = os.environ.get("PR_NUMBER")
-pr_description = os.environ.get("PR_BODY")
+pr_description = "\n" + os.environ.get("PR_BODY")
 
 # check title length with (PR number) added at end
 new_title = title + " (#" + pr_number_string + ")"
