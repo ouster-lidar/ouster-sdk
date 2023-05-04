80be203dc2c9dc730d2d674e692d65b73c87f710
Kai Wong <44489146+kairenw@users.noreply.github.com>
2023-05-04 15:29:43 -0700

    Fix

--- .github/check_title_and_description.py
+++ .github/check_title_and_description.py
@@ -3,12 +3,12 @@ import sys
 import re
 
 title_re = r'\A[0-9A-Z][\x20-\x7E]{0,88}([a-zA-Z0-9]|[\)])'
-description_re = r'((\n[\20-\x7E]{0,100})+)?(\n)?'
+description_re = r'([\20-\x7E]{0,100}\n)+)?(\n)?'
 
 num_commits = int(os.environ.get("NUM_COMMITS"))
 title = os.environ.get("TITLE")
 pr_number_string = os.environ.get("PR_NUMBER")
-pr_description = "\n" + os.environ.get("PR_BODY")
+pr_description = os.environ.get("PR_BODY")
 
 # check title length with (PR number) added at end
 new_title = title + " (#" + pr_number_string + ")"
@@ -28,7 +28,7 @@ if not re.fullmatch(title_re, new_title):
     error = True
 
 if not re.fullmatch(description_re, pr_description):
-    print(f"Error: Please revise your PR description {pr_description} to match the regex {description_re}")
+    print(f"Error: Please revise your PR description {pr_description}\n to match the regex {description_re}")
     error = True
 
 # No need to check two new lines since it is determined by GH Merge settings
