c1722e1dff7f9e09cb21c31008ecacbe779f10ce
Chris Bayruns <chris.bayruns@ouster.io>
2023-09-27 20:12:15 +0000

    Better Discover Connect Support
    * Now uses ip addresses as well as host names
    * Better script encapsulation
    * Changed to threaded sensor information reqs
    * Moved error after the sensor line

--- python/tests/test_discover.py
+++ python/tests/test_discover.py
@@ -6,6 +6,7 @@
 """
 
 import requests
+import ouster.cli.plugins.discover
 from ouster.cli.plugins.discover import service_info_as_text_str
 import socket
 from zeroconf import DNSAddress
@@ -43,17 +44,30 @@ def test_service_info_as_text_str(monkeypatch):
     """It should format correctly even with no addresses in the info."""
     def mock_get(_):
         raise RuntimeError("err")
+    def mock_connect(foo, bar):  # NOQA
+        class MockSocket:
+            def __init__(self, af=None, sock_type=None):  # NOQA
+                pass
+
+            def connect_ex(self, *arg):  # NOQA
+                return 1
+
+            def close(self):
+                pass
+        return MockSocket()
+
     with monkeypatch.context() as m:
         m.setattr(requests, "get", mock_get)
+        m.setattr(socket, "socket", mock_connect)
 
-        text, color = service_info_as_text_str(FakeInfo(FAKESERVER, []))
+        text, color, error = service_info_as_text_str(FakeInfo(FAKESERVER, []))
         server, address, prod_line, dest_ip, lidar_port, imu_port = text.split()
         assert FAKESERVER == server
         assert address == '-'
         assert prod_line == '-'
 
         fake_addresses = ["192.168.100.200", "200a:aa8::8a2e:370:1337"]
-        text, color = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
+        text, color, error = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
         server, address, prod_line, dest_ip, lidar_port, imu_port = text.split()
         assert FAKESERVER == server
         assert address == fake_addresses[0]
@@ -64,11 +78,23 @@ def test_service_info_as_text_str_2(monkeypatch):
     """It should format correctly even when sensor metadata can't be retrieved."""
     def mock_get(_):
         raise RuntimeError("err")
+    def mock_connect(foo, bar):  # NOQA
+        class MockSocket:
+            def __init__(self, af=None, sock_type=None):  # NOQA
+                pass
+
+            def connect_ex(self, *arg):  # NOQA
+                return 1
+
+            def close(self):
+                pass
+        return MockSocket()
     with monkeypatch.context() as m:
         m.setattr(requests, "get", mock_get)
+        m.setattr(socket, "socket", mock_connect)
 
         fake_addresses = ["192.168.100.200", "200a:aa8::8a2e:370:1337"]
-        text, color = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
+        text, color, error = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
         server, address, prod_line, dest_ip, lidar_port, imu_port = text.split()
         assert FAKESERVER == server
         assert address == fake_addresses[0]
@@ -82,12 +108,29 @@ def test_service_info_as_text_str_3(monkeypatch):
             def json(self):
                 return {'prod_line': 'fake_prod_line'}
         return MockResponse()
+    def mock_connect(foo, bar):  # NOQA
+        class MockSocket:
+            def __init__(self, af=None, sock_type=None):  # NOQA
+                pass
+
+            def connect_ex(self, *arg):  # NOQA
+                return 0
+
+            def close(self):
+                pass
+        return MockSocket()
+
+    def mock_config(self, *arg):  # NOQA
+        return None
 
     with monkeypatch.context() as m:
         m.setattr(requests, "get", mock_get)
+        m.setattr(socket, "socket", mock_connect)
+        m.setattr(ouster.cli.plugins.discover, "_get_config", mock_config)
 
         fake_addresses = ["192.168.100.200", "200a:aa8::8a2e:370:1337"]
-        text, color = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
+
+        text, color, error = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
         server, address, prod_line, dest_ip, lidar_port, imu_port = text.split()
         assert FAKESERVER == server
         assert address == fake_addresses[0]
@@ -101,13 +144,66 @@ def test_service_info_as_text_str_4(monkeypatch):
             def json(self):
                 return {}
         return MockResponse()
+    def mock_connect(foo, bar):  # NOQA
+        class MockSocket:
+            def __init__(self, af=None, sock_type=None):  # NOQA
+                pass
+
+            def connect_ex(self, *arg):
+                return 0
+
+            def close(self):
+                pass
+        return MockSocket()
+
+    def mock_config(self, *arg):  # NOQA
+        return None
 
     with monkeypatch.context() as m:
         m.setattr(requests, "get", mock_get)
+        m.setattr(socket, "socket", mock_connect)
+        m.setattr(ouster.cli.plugins.discover, "_get_config", mock_config)
 
         fake_addresses = ["192.168.100.200", "200a:aa8::8a2e:370:1337"]
-        text, color = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
+        text, color, error = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
         server, address, prod_line, dest_ip, lidar_port, imu_port = text.split()
         assert FAKESERVER == server
         assert address == fake_addresses[0]
         assert prod_line == '-'
+
+
+def test_ipv6_url(monkeypatch):
+    """It should produce the correct url"""
+    test_ipv6_address = "200a:aa8::8a2e:370:1337"
+    test_url = f"http://[{test_ipv6_address}]/api/v1/sensor/metadata/sensor_info"
+
+    def mock_get(url):
+        class MockResponse:
+            def json(self):
+                assert test_url == url
+                return None
+
+        return MockResponse()
+
+    def mock_connect(foo, bar):  # NOQA
+        class MockSocket:
+            def __init__(self, af=None, sock_type=None):  # NOQA
+                pass
+
+            def connect_ex(self, *arg):
+                return 0
+
+            def close(self):
+                pass
+        return MockSocket()
+
+    def mock_config(self, *arg):  # NOQA
+        return None
+
+    with monkeypatch.context() as m:
+        m.setattr(requests, "get", mock_get)
+        m.setattr(socket, "socket", mock_connect)
+        m.setattr(ouster.cli.plugins.discover, "_get_config", mock_config)
+
+        fake_addresses = [test_ipv6_address]
+        _, _, _ = service_info_as_text_str(FakeInfo(FAKESERVER, fake_addresses))
