1ad238d71f718daa4c036079f378cd8a1cc652f2
Chris Bayruns <chris.bayruns@ouster.io>
2024-01-04 00:11:40 +0000

    Temporarily fix dep issues

--- python/setup.py
+++ python/setup.py
@@ -165,7 +165,8 @@ def install_requires():
     env = os.environ.copy()
     skip_mapping = env.get('OUSTER_SDK_SKIP_MAPPING')
     if not skip_mapping:
-        install_requires.append('ouster-mapping>=0.2.0.dev3; python_version >= "3.8"')
+        # Once ouster metapackage is done, remove the less than 3.12
+        install_requires.append('ouster-mapping>=0.0.1.dev10; python_version >= "3.8" and python_version < "3.12"')
 
     return install_requires
 
